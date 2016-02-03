c2d75b5126e41d696921d686191a3e7d42379c1e
Tommi Virtanen <tv@eagain.net>
2016-02-03 11:47:45 -0800

    Test ReadDir of a repo

--- src/pfs/fuse/filesystem_test.go
+++ src/pfs/fuse/filesystem_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -122,6 +123,127 @@ func TestRootReadDir(t *testing.T) {
 	}
 	// TODO show RepoInfo.Created as time
 	// if g, e := fi.ModTime().UTC(), repoModTime; g != e {
+	// 	t.Errorf("wrong mtime: %v != %v", g, e)
+	// }
+}
+
+func TestRepoReadDir(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(panicOnFail{t})
+	defer ctrl.Finish()
+
+	client := mock_pfs.NewMockAPIClient(ctrl)
+
+	const (
+		repoName = "foo"
+		repoSize = 42
+	)
+	repoModTime := time.Date(2016, 1, 2, 13, 14, 15, 123456789, time.UTC)
+
+	client.EXPECT().InspectRepo(gomock.Any(), gomock.Eq(&pfs.InspectRepoRequest{
+		Repo: &pfs.Repo{Name: repoName},
+	})).Return(
+		&pfs.RepoInfo{
+			Repo: &pfs.Repo{
+				Name: repoName,
+			},
+			Created:   &google_protobuf.Timestamp{repoModTime.Unix(), int32(repoModTime.Nanosecond())},
+			SizeBytes: repoSize,
+		},
+		error(nil),
+	)
+
+	const (
+		commitID   = "blargh"
+		commitSize = 13
+	)
+	commitStartTime := time.Date(2016, 1, 2, 13, 14, 2, 234567890, time.UTC)
+	commitFinishTime := time.Date(2016, 1, 2, 13, 14, 7, 345678901, time.UTC)
+
+	client.EXPECT().ListCommit(gomock.Any(), gomock.Eq(&pfs.ListCommitRequest{
+		Repo: []*pfs.Repo{
+			{Name: repoName},
+		},
+		CommitType: pfs.CommitType_COMMIT_TYPE_NONE,
+		FromCommit: nil,
+		Block:      false,
+	})).Return(
+		&pfs.CommitInfos{
+			CommitInfo: []*pfs.CommitInfo{
+				{
+					Commit: &pfs.Commit{
+						Repo: &pfs.Repo{
+							Name: repoName,
+						},
+						Id: commitID,
+					},
+					CommitType:   pfs.CommitType_COMMIT_TYPE_READ,
+					ParentCommit: nil,
+					Started:      &google_protobuf.Timestamp{commitStartTime.Unix(), int32(commitStartTime.Nanosecond())},
+					Finished:     &google_protobuf.Timestamp{commitFinishTime.Unix(), int32(commitFinishTime.Nanosecond())},
+					SizeBytes:    repoSize,
+				},
+			},
+		},
+		error(nil),
+	)
+
+	client.EXPECT().InspectCommit(gomock.Any(), gomock.Eq(&pfs.InspectCommitRequest{
+		Commit: &pfs.Commit{
+			Repo: &pfs.Repo{
+				Name: repoName,
+			},
+			Id: commitID,
+		},
+	})).Return(
+		&pfs.CommitInfo{
+			Commit: &pfs.Commit{
+				Repo: &pfs.Repo{
+					Name: repoName,
+				},
+				Id: commitID,
+			},
+			CommitType:   pfs.CommitType_COMMIT_TYPE_READ,
+			ParentCommit: nil,
+			Started:      &google_protobuf.Timestamp{commitStartTime.Unix(), int32(commitStartTime.Nanosecond())},
+			Finished:     &google_protobuf.Timestamp{commitFinishTime.Unix(), int32(commitFinishTime.Nanosecond())},
+			SizeBytes:    repoSize,
+		},
+		error(nil),
+	)
+
+	shard := &pfs.Shard{}
+	commitMounts := []*pfuse.CommitMount{}
+	filesys := pfuse.NewFilesystem(client, shard, commitMounts)
+
+	mnt, err := fstestutil.MountedT(t, filesys, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mnt.Close()
+
+	fis, err := ioutil.ReadDir(filepath.Join(mnt.Dir, repoName))
+	if err != nil {
+		t.Fatalf("readdir: %v", err)
+	}
+	if len(fis) != 1 {
+		t.Fatalf("expected one commit: %v", fis)
+	}
+	fi := fis[0]
+	if g, e := fi.Name(), commitID; g != e {
+		t.Errorf("wrong name: %q != %q", g, e)
+	}
+	// TODO show commitSize in commit stat?
+	if g, e := fi.Size(), int64(0); g != e {
+		t.Errorf("wrong size: %v != %v", g, e)
+	}
+	if g, e := fi.Mode(), os.ModeDir|0555; g != e {
+		t.Errorf("wrong mode: %v != %v", g, e)
+	}
+	// TODO show CommitInfo.StartTime as ctime, CommitInfo.Finished as mtime
+	// TODO test ctime via .Sys
+	// if g, e := fi.ModTime().UTC(), commitFinishTime; g != e {
 	// 	t.Errorf("wrong mtime: %v != %v", g, e)
 	// }
 }
